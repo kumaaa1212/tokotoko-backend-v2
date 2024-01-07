660107e2d7c89d925788ee1cbc4ec48e92f232a8
Ryohei <harrry.kuma1225@gmail.com>
2024-01-08 01:48:17 +0900

    fix database

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,14 +2,14 @@ import { DataSource } from 'typeorm';
 import { User } from './entities/user.entity';
 
 export const AppDataSource = new DataSource({
-  type: 'mysql',
-  host: process.env.DATA_BASE_HOST,
-  port: Number(process.env.DATA_BASE_PORT),
-  username: process.env.DATA_BASE_USER,
-  password: process.env.DATA_BASE_PASSWORD,
-  database: process.env.DATA_BASE_NAME,
-  logging: true,
-  synchronize: false,
-  entities: [User],
-  migrations: ['dist/migration/*.js'],
+  type: 'mysql', // MySQL の場合
+  host: '127.0.0.1', // docker-compose.yml で指定したコンテナの service 名
+  port: 3306, // ポート番号
+  username: 'user', // docker-compose.yml の MYSQL_USER
+  password: 'password', // docker-compose.yml の MYSQL_PASSWORD
+  database: 'sample', // docker-compose.yml の MYSQL_DATABASE
+  logging: true, // コンソール画面に実行したSQLが表示される
+  synchronize: false, // true にすると migration が自動で実行されます。
+  entities: [User], // エンティティクラスを指定する（複数の場合はカンマで区切る）
+  migrations: ['dist/migration/*.js'], // dist ディレクトリ内の js ファイルを指定する
 });
